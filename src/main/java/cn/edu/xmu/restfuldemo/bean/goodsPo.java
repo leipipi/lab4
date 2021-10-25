10aac46f1682f555831e42b6d6237a4937b6a4d9
leipipi <1195389634@qq.com>
2021-10-25 20:39:21 +0800

    修改项目结构 重命名一些东西

---
+++ src/main/java/cn/edu/xmu/restfuldemo/bean/goodsPo.java
@@ -0,0 +1,27 @@
+package cn.edu.xmu.restfuldemo.bean;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class goodsPo {
+    private Integer id;
+    private Integer goods_spu_id;
+    private String sku_sn;
+    private String name;
+    private Integer original_price;
+    private String configuration;
+    private Integer weight;
+    private String image_url;
+    private Integer inventory;
+    private String detail;
+    private Integer disavled;
+    private Date gmt_create;
+    private Date gmt_modified;
+    private Integer state;
+}
