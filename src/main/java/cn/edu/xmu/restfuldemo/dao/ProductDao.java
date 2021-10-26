6fc354634cfa66152b261acc442dacdddb07c445
leipipi <1195389634@qq.com>
2021-10-26 19:30:13 +0800

    更新productdao

---
+++ src/main/java/cn/edu/xmu/restfuldemo/dao/ProductDao.java
@@ -0,0 +1,17 @@
+package cn.edu.xmu.restfuldemo.dao;
+
+import cn.edu.xmu.restfuldemo.mapper.ProductMapper;
+import cn.edu.xmu.restfuldemo.bean.*;
+//import cn.edu.xmu.restfuldemo.util.Common;
+import cn.edu.xmu.restfuldemo.bean.ProductsVo;
+import cn.edu.xmu.restfuldemo.util.ReturnObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDao {
+}
