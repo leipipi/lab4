b6f25c5ad556e3ef9a28ad08fc31793325c4c65d
chenye <545944937@qq.com>
2021-10-26 16:25:37 +0800

    更新GoodsMapper

---
+++ src/main/java/cn/edu/xmu/restfuldemo/dao/GoodsDao.java
@@ -0,0 +1,20 @@
+package cn.edu.xmu.restfuldemo.dao;
+
+import cn.edu.xmu.restfuldemo.mapper.OrderMapper;
+import cn.edu.xmu.restfuldemo.bean.*;
+//import cn.edu.xmu.restfuldemo.util.Common;
+import cn.edu.xmu.restfuldemo.bean.OrdersVo;
+import cn.edu.xmu.restfuldemo.util.ReturnObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public class GoodsDao {
+    private Logger logger= LoggerFactory.getLogger(GoodsDao.class);
+}
