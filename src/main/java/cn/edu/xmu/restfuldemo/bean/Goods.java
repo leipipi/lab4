325f84f83a92d3af805004eb57c63e8173abc92e
leipipi <1195389634@qq.com>
2021-10-26 16:21:39 +0800

    xiugai

--- src/main/java/cn/edu/xmu/restfuldemo/bean/Goods.java
+++ src/main/java/cn/edu/xmu/restfuldemo/bean/Goods.java
@@ -14,4 +14,31 @@ public class Goods implements VoObject {
     public Goods(GoodsPo goodsPo){this.goodsPo=goodsPo;}
     private GoodsPo goodsPo;
 
+    public Integer getId(){return goodsPo.getId();}
+    public void setId(Integer id){goodsPo.setId(id);}
+
+    public Integer getGoods_sku_id(){return goodsPo.getGoods_sku_id();}
+    public void setGoods_sku_id(Integer id){goodsPo.setGoods_sku_id(id);}
+
+    public String getSku_sn(){return goodsPo.getSku_sn();}
+    public void setSku_sn(String Sku_sn){goodsPo.setSku_sn(Sku_sn);}
+
+    public String getName(){return goodsPo.getName();}
+    public void setName(String Name){goodsPo.setName(Name);}
+
+    public Integer getOriginal_price(){return goodsPo.getOriginal_price();}
+    public void setOriginal_price(Integer original_price){goodsPo.setOriginal_price(original_price);}
+
+    public String getConfiguration(){return goodsPo.getConfiguration();}
+    public void setConfiguration(String configuration){goodsPo.setConfiguration(configuration);}
+
+    public Integer getWeight(){return goodsPo.getWeight();}
+    public void setWeight(Integer weight){goodsPo.setWeight(weight);}
+
+    public String getImage_url(){return goodsPo.getImage_url();}
+    public void setImage_url(String image_url){goodsPo.setImage_url(image_url);}
+
+    public Integer getInventory(){return goodsPo.getInventory();}
+    public void setInventory(Integer inventory){goodsPo.setInventory(inventory);}
+
 }
