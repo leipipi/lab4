426870b0b8ff39d5fa03c2a2cf6e74efa1480200
leipipi <1195389634@qq.com>
2021-10-27 22:21:09 +0800

    删除无用类

--- src/main/java/cn/edu/xmu/restfuldemo/dao/GoodsDao.java
+++ src/main/java/cn/edu/xmu/restfuldemo/dao/GoodsDao.java
@@ -1,17 +1,9 @@
 package cn.edu.xmu.restfuldemo.dao;
 
-import cn.edu.xmu.restfuldemo.mapper.OrderMapper;
-import cn.edu.xmu.restfuldemo.bean.*;
 //import cn.edu.xmu.restfuldemo.util.Common;
-import cn.edu.xmu.restfuldemo.bean.OrdersVo;
-import cn.edu.xmu.restfuldemo.util.ReturnObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
+        import org.springframework.stereotype.Repository;
 
 
 @Repository
